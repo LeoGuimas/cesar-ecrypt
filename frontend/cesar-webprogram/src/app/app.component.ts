45bfad7cfaa11b9cf04d4d2ce3ad0f0c5cafaa45
LeoGuimas <gleo.dev0@gmail.com>
2024-10-30 11:03:37 -0300

    feat: buttom logic and some scss

--- frontend/cesar-webprogram/src/app/app.component.ts
+++ frontend/cesar-webprogram/src/app/app.component.ts
@@ -2,11 +2,20 @@ import { Component } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { DecryptComponent } from "./components/decrypt/decrypt.component";
 import { EncryptComponent } from "./components/encrypt/encrypt.component";
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
   standalone: true,
-  imports: [DecryptComponent, EncryptComponent]
+  imports: [CommonModule, DecryptComponent, EncryptComponent]
 })
-export class AppComponent { }
+export class AppComponent { 
+  title = 'cesar-webprogram';
+  showEncrypt = true;
+
+  toggleComponent(){
+    this.showEncrypt = !this.showEncrypt;
+  }
+}
 
